594b3f40507c410ca724489f5129be03a722a6d3
Milagu <46215198+Milagu@users.noreply.github.com>
2019-05-24 21:56:10 -0700

    Replacing workable version.

--- app/data_functions.py
+++
@@ -1,7 +0,0 @@
-import pandas
-from sqlalchemy import create_engine
-
-def engine():
-    rds_connection_string = "root:root@127.0.0.1/customer_db"
-    engine = create_engine(f'mysql+pymysql://{rds_connection_string}')
-    return engine
